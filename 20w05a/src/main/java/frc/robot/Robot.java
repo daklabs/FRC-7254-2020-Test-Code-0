c8293671752b0b109dd14fd53c444d2551204fb2
Potterville Knowmads <pottervilleknowmadsrobotics@gmail.com>
2020-02-15 12:45:15 -0500

    Formating changes

--- 20w05a/src/main/java/frc/robot/Robot.java
+++ 20w05a/src/main/java/frc/robot/Robot.java
@@ -80,9 +80,7 @@ public class Robot extends TimedRobot {
     // THe farther from the object, the less accurate it is.
     Color detectedColor = m_colorSensor.getColor();
 
-    /**
-     * Run the color match algorithm on our detected color
-     */
+    // Run the color match algorithm on our detected color
     String colorString;
     ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
 
@@ -98,10 +96,7 @@ public class Robot extends TimedRobot {
       colorString = "Unknown";
     }
 
-    /**
-     * Open Smart Dashboard or Shuffleboard to see the color detected by the 
-     * sensor.
-     */
+    // Open Smart Dashboard or Shuffleboard to see the color detected by the sensor.
     SmartDashboard.putNumber("Red", detectedColor.red);
     SmartDashboard.putNumber("Green", detectedColor.green);
     SmartDashboard.putNumber("Blue", detectedColor.blue);
